22aae5e70785a85d57a7fff61e1e30a6eabc2cd7
Emerson Carvalho <emersoncarvalhoss11@outlook.com>
2022-11-26 14:58:20 -0300

    Commit

---
+++ src/main/kotlin/desafios/crescimentoPopulacional/Pais.kt
@@ -0,0 +1,23 @@
+package desafios.crescimentoPopulacional
+
+data class Pais(var habitantes: Double, val taxaCrescimento: Double) {
+    fun crescerPopulacaoAnual() {
+        habitantes += (taxaCrescimento / 100 * habitantes)
+    }
+}
+
+fun main() {
+    val habitantesPaisA = readLine()!!.toDouble();
+    val habitantesPaisB = readLine()!!.toDouble();
+    val paisA = Pais(habitantesPaisA, taxaCrescimento = 3.0)
+    val paisB = Pais(habitantesPaisB, taxaCrescimento = 1.5)
+
+    var quantidadeAnos = 0
+    while (paisA.habitantes < paisB.habitantes) {
+        paisA.crescerPopulacaoAnual()
+        paisB.crescerPopulacaoAnual()
+        quantidadeAnos++
+    }
+
+    println("$quantidadeAnos anos")
+}
